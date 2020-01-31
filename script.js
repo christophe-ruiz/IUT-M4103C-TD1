dd98a57b1a6cb090c10a24cc9267f9e49f262956
Christophe Ruiz <christophe.ruiz@etu.univ-amu.fr>
2020-01-31 12:06:39 +0100

    debut damier

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log('test');
+(function () {
+    $(document).ready(function () {
+        /*
+        console.log('DEBUT');
+        for (let i = 0; i < 10; ++i) {
+            $("#element").fadeOut(1000,
+                function () {
+                    console.log('out fini')
+                });
+            $("#element").fadeIn(1000,
+                function () {
+                    console.log('in fini')
+                });
+        }
+        console.log('FIN');
+        */
+
+        for (let i = 0; i < 8; ++i) {
+            let bloc = $("<div>&nbsp;</div>").css({'width':'5vw', 'height':'5vw', 'background':'white', 'border':'solid 1px red'});
+            $('#titre').append(bloc);
+        }
+    });
+})();
