f1da4587ad500cdfff79f98f772eb2bbf75fd225
Christophe Ruiz <christophe.ruiz@etu.univ-amu.fr>
2020-02-01 10:02:12 +0100

    damier généré

--- script.js
+++ script.js
@@ -13,11 +13,27 @@
                 });
         }
         console.log('FIN');
+
+
         */
 
-        for (let i = 0; i < 8; ++i) {
-            let bloc = $("<div>&nbsp;</div>").css({'width':'5vw', 'height':'5vw', 'background':'white', 'border':'solid 1px red'});
-            $('#titre').append(bloc);
+        let lines = []
+        for (let j = 0; j < 8; ++j) {
+            lines.push($("<div></div>").css({'width':'40vw', 'height':'5vw', 'display':'flex'}));
+        }
+        for (let j = 0; j < lines.length; ++j) {
+            if (j%2==0) {
+                for (let i = 0; i < lines.length/2; i++) {
+                    $(lines[j]).append($("<div>&nbsp;</div>").css({'width':'5vw', 'height':'5vw', 'background':'white', 'border':'solid 1px black', 'display':'flex'}))
+                    $(lines[j]).append($("<div>&nbsp;</div>").css({'width':'5vw', 'height':'5vw', 'background':'black', 'border':'solid 1px black', 'display':'flex'}))
+                }
+            } else {
+                for (let i = 0; i < lines.length/2; i++) {
+                    $(lines[j]).append($("<div>&nbsp;</div>").css({'width':'5vw', 'height':'5vw', 'background':'black', 'border':'solid 1px black', 'display':'flex'}))
+                    $(lines[j]).append($("<div>&nbsp;</div>").css({'width':'5vw', 'height':'5vw', 'background':'white', 'border':'solid 1px black', 'display':'flex'}))
+                }
+            }
+            $('body').append(lines[j]);
         }
     });
 })();
