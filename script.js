af4bfeef6e3ae59633440ff89d47b5a8f1d65806
Christophe Ruiz <christophe.ruiz@etu.univ-amu.fr>
2020-02-04 14:08:05 +0100

    code propre, je crois.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 (function () {
-    $(document).ready(function () {
+    $(function () {
         let lines = [];
         let cssBlack =  {'width':'5vw', 'height':'5vw', 'background':'black', 'border':'solid 1px black', 'display':'flex'};
         let cssWhite = {'width':'5vw', 'height':'5vw', 'background':'white', 'border':'solid 1px black', 'display':'flex'};
@@ -8,17 +8,16 @@
         }
         for (let i = 0; i < lines.length; ++i) {
             for (let j = 0; j < lines.length/2; j++) {
-                if (i%2==0) {
-                    $(lines[i])
-                        .append($("<div>&nbsp;</div>").css(cssWhite))
-                        .append($("<div>&nbsp;</div>").css(cssBlack));
-                } else {
-                    $(lines[i])
-                        .append($("<div>&nbsp;</div>").css(cssBlack))
-                        .append($("<div>&nbsp;</div>").css(cssWhite));
-                }
+                $(lines[i])
+                    .append($("<div>&nbsp;</div>").css(i%2==0 ? cssWhite : cssBlack))
+                    .append($("<div>&nbsp;</div>").css(i%2!=0 ? cssWhite : cssBlack));
             }
             $('body').append(lines[i]);
         }
     });
+
+    $(function () {
+        let cssBlack =  {'width':'5vw', 'height':'5vw', 'background':'black', 'border':'solid 1px black', 'display':'flex'};
+        let cssWhite = {'width':'5vw', 'height':'5vw', 'background':'white', 'border':'solid 1px black', 'display':'flex'};
+    })
 })();
