62af6b73be9fc18e5b221707322d1ff76d5dc99c
Christophe Ruiz <christophe.ruiz@etu.univ-amu.fr>
2020-02-04 14:47:25 +0100

    ça change de couleur quand on hover

--- script.js
+++ script.js
@@ -10,8 +10,16 @@
         for (let i = 0; i < lines.length; ++i) {
             for (let j = 0; j < lines.length/2; j++) {
                 $(lines[i])
-                    .append($("<div>&nbsp;</div>").css(i%2==0 ? cssWhite : cssBlack))
-                    .append($("<div>&nbsp;</div>").css(i%2!=0 ? cssWhite : cssBlack));
+                    .append($("<div>&nbsp;</div>").css(i%2==0 ? cssWhite : cssBlack).mouseenter(function () {
+                        $(this).css('background', 'red');
+                    }).mouseleave(function () {
+                        $(this).css('background', i%2==0 ? 'white' : 'black');
+                    }))
+                    .append($("<div>&nbsp;</div>").css(i%2!=0 ? cssWhite : cssBlack).mouseenter(function () {
+                        $(this).css('background', 'red');
+                    }).mouseleave(function () {
+                        $(this).css('background', i%2!=0 ? 'white' : 'black');
+                    }))
             }
             $('body').append(lines[i]);
         }
