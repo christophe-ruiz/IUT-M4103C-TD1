14358958d2b4769e0f66505fbae7ceeaae98ef51
Christophe Ruiz <christophe.ruiz@etu.univ-amu.fr>
2020-02-04 13:58:51 +0100

    code propre, je crois.

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
 (function () {
     $(document).ready(function () {
         let lines = [];
+        let cssBlack =  {'width':'5vw', 'height':'5vw', 'background':'black', 'border':'solid 1px black', 'display':'flex'};
+        let cssWhite = {'width':'5vw', 'height':'5vw', 'background':'white', 'border':'solid 1px black', 'display':'flex'};
         for (let i = 0; i < 8; ++i) {
             lines.push($("<div></div>").css({'width':'40vw', 'height':'5vw', 'display':'flex'}));
         }
         for (let i = 0; i < lines.length; ++i) {
             for (let j = 0; j < lines.length/2; j++) {
                 if (i%2==0) {
-                $(lines[i])
-                    .append($("<div>&nbsp;</div>").css({'width':'5vw', 'height':'5vw', 'background':'white', 'border':'solid 1px black', 'display':'flex'}))
-                    .append($("<div>&nbsp;</div>").css({'width':'5vw', 'height':'5vw', 'background':'black', 'border':'solid 1px black', 'display':'flex'}))
+                    $(lines[i])
+                        .append($("<div>&nbsp;</div>").css(cssWhite))
+                        .append($("<div>&nbsp;</div>").css(cssBlack));
                 } else {
-                $(lines[i])
-                    .append($("<div>&nbsp;</div>").css({'width':'5vw', 'height':'5vw', 'background':'black', 'border':'solid 1px black', 'display':'flex'}))
-                    .append($("<div>&nbsp;</div>").css({'width':'5vw', 'height':'5vw', 'background':'white', 'border':'solid 1px black', 'display':'flex'}))
+                    $(lines[i])
+                        .append($("<div>&nbsp;</div>").css(cssBlack))
+                        .append($("<div>&nbsp;</div>").css(cssWhite));
                 }
             }
             $('body').append(lines[i]);
